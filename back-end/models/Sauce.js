c18fc223b164f14a492f81b93ca32f41cc81ab5c
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-10-26 16:26:21 +0200

    Renomer le fichier Thing (chose)
    replacer Thing par sauce

--- back-end/models/Sauce.js
+++ back-end/models/Sauce.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-const thingSchema = mongoose.Schema({
+const sauceSchema = mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String, required: true },
   userId: { type: String, required: true },
   price: { type: number, required: true },
 });
-module.experts = mongoose.model("Thing", thingSchema);
+module.experts = mongoose.model("Thing", sauceSchema);
