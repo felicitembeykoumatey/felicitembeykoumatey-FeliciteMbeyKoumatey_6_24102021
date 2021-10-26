1b91cdbd7572ab6ec5bc19296955fb1dd452962a
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-10-26 17:50:26 +0200

    suppression ligne vide au dessus de la const rateLimit

---
+++ back-end/controllers/user.js
@@ -0,0 +1,71 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const maskData = require("maskdata");
+const passwordValidator = require("password-validator");
+
+const schemaPassValid = new passwordValidator();
+
+schemaPassValid
+  .is()
+  .min(8)
+  .is()
+  .max(50)
+  .has()
+  .uppercase()
+  .has()
+  .lowercase()
+  .has()
+  .digits(2)
+  .has()
+  .not()
+  .spaces()
+  .is()
+  .not()
+  .oneOf(["Passw0rd", "Password123"]);
+
+exports.signup = (req, res, next) => {
+  if (!schemaPassValid.validate(req.body.password)) {
+    res.status(401).json({
+      message:
+        "Sécurité du mot de passe faible. Il doit contenir au moins 8 caractère, des majuscules et deux chiffres",
+    });
+  }
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: maskData.maskEmail2(req.body.email),
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
+
+exports.login = (req, res, next) => {
+  User.findOne({ email: maskData.maskEmail2(req.body.email) })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: "Utilisateur non trouvé !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
