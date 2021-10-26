8def7a076a5034e7fd8a8c42f2bce64abbb8dc70
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-10-26 15:39:06 +0200

    ajout fichier app.js

---
+++ back-end/app.js
@@ -0,0 +1,93 @@
+require("dotenv").config();
+app.use("/api/stuff", stuffRoutes);
+/**déclaration constances */
+const bodyParser = require("body-parser");
+const Thing = require("./models/thing");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+//** utiliser body-parser par Json */
+
+app.use(bodyParser.json());
+
+// methode Post**/
+app.post("/api/stuff", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body,
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+/** Méthode GET connexion à stuff.js par use */
+app.use("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
+});
+
+/**Une fois installer MongoDB, connexion à mongooseDB */
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    `
+    mongodb+srv://felicite_mbeykoumatey21:Mbeyfeli1990@cluster0.qbnko.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+app.use(helmet());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
