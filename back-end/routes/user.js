3b3d63ebcac965176af0809a2e51bb3f9f65f181
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-10-26 17:52:16 +0200

    modification code dans le fichier user.js
    suppression ligne vide

---
+++ back-end/routes/user.js
@@ -0,0 +1,31 @@
+exports.signup = (req, res, next) => {};
+
+exports.login = (req, res, next) => {};
+const express = require("express");
+const router = express.Router();
+
+const userCtrl = require("../controllers/user");
+
+router.post("/signup", userCtrl.signup);
+router.post("/login", userCtrl.login);
+
+module.exports = router;
+const userRoutes = require("./routes/user");
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
+
+exports.signup = (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
