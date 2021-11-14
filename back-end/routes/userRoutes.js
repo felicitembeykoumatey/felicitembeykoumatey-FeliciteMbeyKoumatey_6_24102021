de7017cd5849a754cddd7369fbd0016f6829579b
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-11-14 12:09:13 +0100

    ajout limiter dans userRoutes

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const limiter = require("../middleware/limiter");
 
 const userCtrl = require("../controllers/user");
 const password = require("../middleware/password");
