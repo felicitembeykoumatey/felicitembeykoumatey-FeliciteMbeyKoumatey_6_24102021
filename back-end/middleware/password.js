d1fec3a9b2e7c8c3e7a4ed5a375285475f2af24f
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-11-14 19:42:03 +0100

    suppressionfichier password

--- back-end/middleware/password.js
+++
@@ -1,9 +0,0 @@
-const schema = require("../models/password");
-
-module.exports = (req, res, next) => {
-  if (!schema.validate(req.body.password)) {
-    return res.status(400).json("Votre mot de passe est trop faible");
-  } else {
-    next();
-  }
-};
