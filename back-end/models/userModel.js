a1d5e7ce49a52d30d6a0d4aeef97ccd7c9c9630f
MBEY KOUMATEY <felicitembeykoumatey@gmail.com>
2021-11-15 12:09:19 +0100

    supprimer console.log(userSchema)

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -7,12 +7,10 @@ const uniqueValidator = require("mongoose-unique-validator");
 /**Dans notre schéma, la valeur unique , avec l'élément mongoose-unique-validator passé comme plug-in,
  * s'assurera qu'aucun des deux utilisateurs ne peut partager la même adresse e-mail. */
 
-const userSchema = mongoose.Schema ({
+const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
-console.log(userSchema)
-
 
 userSchema.plugin(uniqueValidator);
 //Exportation du module
